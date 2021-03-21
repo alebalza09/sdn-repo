3a37d9f8945309884e2b02292e4a392207e79920
alexandrabalza09@gmail.com <alexandrabalza09@gmail.com>
2021-03-21 12:26:00 -0400

    adding more code to asignacion7.py

--- asignacion7.py
+++ asignacion7.py
@@ -6,3 +6,27 @@ response = requests.post(
     headers={'Authorization':'Basic ZGV2bmV0dXNlcjpDaXNjbzEyMyE='})
 payload=response.json()
 pprint(payload)
+
+res = requests.get(
+    'https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device',
+    headers={'X-Auth-Token': payload['Token']}).json()
+data = res['response']
+
+dash = '-' * 140
+print(dash)
+print("{:^20s} {:^20} {:<35} {:>30} {:>25}".format(
+    'Tipo de Equipos',
+    'Hostname',
+    'Direccion IP de administracion',
+    'Fecha de ultima actualizacion',
+    'Estatus del equipo'))
+print(dash)
+
+for i in data:
+    print('{:^20s} {:^20s} {:^30s} {:^40s} {:^25s}'.format(
+        i['family'],
+        i['hostname'],
+        i['managementIpAddress'],
+        i['lastUpdated'],
+        i['reachabilityStatus']
+    ))
